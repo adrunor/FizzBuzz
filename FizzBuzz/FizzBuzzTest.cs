2b28de4ee5175f213d1064a8b6852045df2c6521
Mickael LANGLET <mika.lgt@outlook.com>
2024-06-25 16:15:15 +0200

    Première étape : Rouge
    Ici on a ni la classe FizzBuzzPrinter ni la méthode Print. Le code ne
    compile pas, c'est normal.

--- FizzBuzz/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzzTest.cs
@@ -4,7 +4,13 @@ namespace FizzBuzz;
 public class FizzBuzzTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void Should_return_same_number_when_it_is_not_multiple_of_three_or_five()
     {
+        const int number = 1;
+            
+        var fizzBuzz = new FizzBuzzPrinter();
+        var value = fizzBuzz.Print(number);
+
+        Assert.Equals("1", value);
     }
 }
