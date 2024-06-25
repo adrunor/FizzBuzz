07a4e8f09ff0b52840f603efa57cded8872e8555
Mickael LANGLET <mika.lgt@outlook.com>
2024-06-25 16:33:20 +0200

    Pas besoin de refactor donc je retourne à l'étape Red
    Le test ne passe pas

--- FizzBuzz/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzzTest.cs
@@ -6,11 +6,25 @@ public class FizzBuzzTest
     [TestMethod]
     public void Should_return_same_number_when_it_is_not_multiple_of_three_or_five()
     {
-        const int number = 1;
-            
         var fizzBuzz = new FizzBuzzPrinter();
+        
+        const int number = 1;
+
         var value = fizzBuzz.Print(number);
 
         Assert.AreEqual("1", value);
+    }
+
+    [TestMethod]
+    public void Should_return_fizz_when_number_is_multiple_of_three()
+    {
+        var fizzBuzz = new FizzBuzzPrinter();
+        
+        const int number = 3;
+
+        var value = fizzBuzz.Print(number);
+        
+        Assert.AreEqual("Fizz", value);
+        
     }
 }
